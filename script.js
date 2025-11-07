352b10dbc3be64cb8f5a9db712646b7865a165b1
rohanborsa6600-tech <rohanborsa6600@gmail.com>
2025-11-07 22:12:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-const routes = {
-  "gita": "gita/index.html",
-  "sthalpothi": "sthalpothi/index.html",
-  "sumanmala": "sumanmala/index.html",
-  "geetapath": "geetapath/index.html",
-  "sutrapath": "sutrapath/index.html",
-  "ashtak": "ashtak/index.html"
-};
-
 function go(name) {
-  window.location.href = routes[name];
+  window.location.href = name + "/index.html";
 }
